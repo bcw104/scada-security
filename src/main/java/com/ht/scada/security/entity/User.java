d28769b669a343c80bdb644d593471653b0f77b8
zhw811028 <zhw811028@vip.qq.com>
2013-05-25 15:47:20 +0800

    用户管理 赵玮

--- src/main/java/com/ht/scada/security/entity/User.java
+++ src/main/java/com/ht/scada/security/entity/User.java
@@ -1,10 +1,6 @@
 package com.ht.scada.security.entity;
 
-import java.util.Set;
-
-import javax.persistence.CollectionTable;
 import javax.persistence.Column;
-import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
@@ -30,26 +26,11 @@ public class User extends AbstractPersistable<Integer> {
 	@Column(nullable=false)
 	private String password;
 	
-	private String name;
-	private String gender;
-	private String department;
-	private String email;
-	private String address;
-	private String realName;
-	
+    private String name;
+    
 	@ManyToOne(fetch=FetchType.EAGER)
 	@JoinColumn(name="role_id")
 	private UserRole userRole;
-	
-	@ElementCollection(fetch=FetchType.LAZY)
-	@CollectionTable(name = "T_User_MajorTag")
-	@Column(name = "major_tag_id")
-	private Set<Integer> majorTagID;// user:edit,user:add
-	
-	@ElementCollection(fetch=FetchType.LAZY)
-	@CollectionTable(name = "T_User_endTag")
-	@Column(name = "end_tag_id")
-	private Set<Integer> endTagID;// user:edit,user:add
 	
 	public User() {
 	}
@@ -66,14 +47,6 @@ public class User extends AbstractPersistable<Integer> {
 		this.username = username;
 	}
 
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
 	public String getPassword() {
 		return password;
 	}
@@ -81,9 +54,18 @@ public class User extends AbstractPersistable<Integer> {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-        public void setShaPassword(String password){
-            this.password=new Sha256Hash(password).toHex();
-        }
+    
+    public void setShaPassword(String password){
+        this.password=new Sha256Hash(password).toHex();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
 	public UserRole getUserRole() {
 		return userRole;
@@ -92,59 +74,4 @@ public class User extends AbstractPersistable<Integer> {
 	public void setUserRole(UserRole userRole) {
 		this.userRole = userRole;
 	}
-    
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getRealName() {
-        return realName;
-    }
-
-    public void setRealName(String realName) {
-        this.realName = realName;
-    }
-    public Set<Integer> getMajorTagID() {
-        return majorTagID;
-    }
-
-    public void setMajorTagID(Set<Integer> majorTagID) {
-        this.majorTagID = majorTagID;
-    }
-
-    public Set<Integer> getEndTagID() {
-        return endTagID;
-    }
-
-    public void setEndTagID(Set<Integer> endTagID) {
-        this.endTagID = endTagID;
-    }
 }
